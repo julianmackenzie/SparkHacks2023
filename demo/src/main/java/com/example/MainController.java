a8e643abc4a16db69b919d57be32932ad02d67b3
JugTBug <julianbmackenzie@gmail.com>
2023-04-08 13:28:01 -0500

    Added beginning of ListView code

--- demo/src/main/java/com/example/MainController.java
+++ demo/src/main/java/com/example/MainController.java
@@ -2,6 +2,10 @@ package com.example;
 
 import java.io.IOException;
 import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 
 public class MainController {
 
@@ -9,4 +13,23 @@ public class MainController {
     private void switchToSettings() throws IOException {
         App.setRoot("settings");
     }
+
+    @FXML
+    BorderPane bp = new BorderPane();
+
+
+    @FXML
+    ListView<VBox> eventList = new ListView<VBox>();
+
+
+    Text t = new Text("Test");
+    VBox v = new VBox(t);
+
+    eventList.getItems().add(v);
+
+
+
+
+
+
 }
