458cc95a793cae0d714768636a133e7622226561
John Hillebrand <john.hillebrand23@gmail.com>
2023-04-08 12:08:33 -0500

    Test

---
+++ demo/src/main/java/com/example/Server.java
@@ -0,0 +1,17 @@
+package com.example;
+
+public class Server {
+    
+
+    public class TheServer extends Thread {
+        public void run() {
+
+        }
+    }
+
+    public class ClientThread extends Thread {
+        public void run() {
+            
+        }
+    }
+}
